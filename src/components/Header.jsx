3c1ee1598421edb9e8c8ab5cdf33dc077c1816ee
Merve Şimşek <93996187+Merve-Simsek92@users.noreply.github.com>
2022-03-28 20:21:15 +0300

    çalışma

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+
+const Header =()=>{
+
+    return(
+    <div className="header">
+        <h1>Task Tracker</h1>
+        <button className="closeButton">Close Add Task Bar</button>
+    </div>)
+
+}
+export default Header;
